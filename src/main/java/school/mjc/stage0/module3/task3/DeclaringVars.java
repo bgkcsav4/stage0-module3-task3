f29f973c04775ca6a9d746c671c0867881546138
Latsa <bgkcsavvy@gmail.com>
2023-07-22 12:19:57 +0200

    done

--- src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
+++ src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.module3.task3;
 
 public class DeclaringVars {
     public static void main(String[] args) {
-        int a = 1, b = 10, c = 100;
+        int a = 1, b = 100, c = 10;
 
         System.out.println(c);
     }
